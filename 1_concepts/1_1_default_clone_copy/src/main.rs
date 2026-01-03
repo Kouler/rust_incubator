20e0d29409d6df4cad2a46c5ff1bfc13b6137d75
Kouler <beeproger@yandex.ru>
2026-01-03 19:44:06 +1000

    1_1 is completed

--- 1_concepts/1_1_default_clone_copy/src/main.rs
+++ 1_concepts/1_1_default_clone_copy/src/main.rs
@@ -1,3 +1,54 @@
+//use smart_default::SmartDefault;
+
+#[derive(Default, Copy, Clone)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Clone)]
+struct Polyline {
+    first: Point,
+    rest: Vec<Point>,
+}
+
+impl Polyline {
+
+    fn new(first: Point, rest: Vec<Point>) -> Self {
+        Self { first, rest }
+    }
+
+    fn from_point(first: Point) -> Self {
+        Self {
+            first,
+            rest: Vec::new(),
+        }
+    }
+
+    fn from_slice(points: &[Point]) -> Option<Self> {
+        if points.is_empty() {
+            None
+        }
+        else {
+            let first = points[0];
+            let rest = points[1..].to_vec();
+            Some ( Self {
+                first,
+                rest
+            })
+        }
+    }
+
+    fn points(&self) -> impl Iterator<Item = &Point> {
+        std::iter::once(&self.first).chain(self.rest.iter())
+    }
+
+    fn push(&mut self, point: Point) {
+        self.rest.push(point);
+    }
+}
+
+
 fn main() {
-    println!("Implement me!");
+
 }
